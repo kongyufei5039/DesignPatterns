f05782c8b29622fcaa57be805faddccc3596372e
kongyufei <kongyufei5039@outlook.com>
2018-07-24 09:48:54 +0800

    实现全局发布订阅模式

--- 5.PublishSubscription.js
+++ 5.PublishSubscription.js
@@ -40,46 +40,87 @@
 
 
 // 对以上模式实现通用方法
+// var event = {
+//     clientList: [], // 缓存列表，存放订阅者的回调函数
 
-var event = {
-    clientList: [], // 缓存列表，存放订阅者的回调函数
+//     listen: function ( key, fn ) { // 订阅接口
+//         if ( !this.clientList[ key ] ) { // 如果没有此类型的订阅，创建一个该类的列表
+//             this.clientList[ key ] = []
+//         }
 
-    listen: function ( key, fn ) { // 订阅接口
-        if ( !this.clientList[ key ] ) { // 如果没有此类型的订阅，创建一个该类的列表
-            this.clientList[ key ] = []
+//         this.clientList[ key ].push( fn ) // 订阅消息添加进消息列表
+//     },
+
+//     trigger: function () { // 发布消息接口
+//         var key = Array.prototype.shift.call( arguments ), // 取出消息类型
+//             fns = this.clientList[ key ];
+//         if ( !fns || fns.length === 0 ) { // 没有订阅该消息，则返回
+//             return false;
+//         }
+//         for ( var i = 0, fn; fn = fns[ i++ ]; ) {
+//             fn.apply( this, arguments ); // arguments 是发布消息时附送的参数
+//         }
+//     }
+// }
+
+// var installEvent = function( obj ) {
+//     for ( var i in event ) {
+//         obj[ i ] = event[ i ];
+//     }
+// }
+
+// var salesOffices = {};
+// installEvent(salesOffices)
+
+// salesOffices.listen('squareMeter88', function( price ) { // 小明订阅88平方米房子的消息
+//     console.log( '价格= ', + price)
+// });
+
+// salesOffices.listen('squareMeter110', function( price ) { // 小红订阅110平方米房子的消息
+//     console.log( '价格= ' + price)
+// });
+
+// salesOffices.trigger( 'squareMeter88', 2000 ); // 发布 88 平方米房子的价格
+// salesOffices.trigger( 'squareMeter110', 3000 ); // 发布 110 平方米房子的价格
+
+
+
+// 实现全局的发布-订阅对象
+var Event = (function () {
+    var clientList = {},
+        listen,
+        trigger,
+        remove;
+    listen = function (key, fn) {
+        if (!clientList[key]) {
+            clientList[key] = []
         }
-
-        this.clientList[ key ].push( fn ) // 订阅消息添加进消息列表
-    },
-
-    trigger: function () { // 发布消息接口
-        var key = Array.prototype.shift.call( arguments ), // 取出消息类型
-            fns = this.clientList[ key ];
-        if ( !fns || fns.length === 0 ) { // 没有订阅该消息，则返回
-            return false;
+        clientList[key].push(fn)
+    }
+    trigger = function () {
+        var key = Array.prototype.shift.call(arguments),
+            fns = clientList[key];
+        if (!fns || fns.length === 0) {
+            return false
         }
-        for ( var i = 0, fn; fn = fns[ i++ ]; ) {
-            fn.apply( this, arguments ); // arguments 是发布消息时附送的参数
+        for (var i=0, fn; fn = fns[i++];) {
+            fn.apply(this, arguments)
         }
     }
-}
-
-var installEvent = function( obj ) {
-    for ( var i in event ) {
-        obj[ i ] = event[ i ];
+    return {
+        listen: listen,
+        trigger: trigger
     }
-}
+})()
 
-var salesOffices = {};
-installEvent(salesOffices)
+Event.listen('squareMeter88', function(price) {
+    console.log('价格: ' + price)
+})
 
-salesOffices.listen('squareMeter88', function( price ) { // 小明订阅88平方米房子的消息
-    console.log( '价格= ', + price)
-});
+Event.listen('squareMeter100', function(price) {
+    console.log('价格: ' + price)
+})
 
-salesOffices.listen('squareMeter110', function( price ) { // 小红订阅110平方米房子的消息
-    console.log( '价格= ' + price)
-});
+Event.trigger('squareMeter88', 4000);
+Event.trigger('squareMeter100', 5000);
 
-salesOffices.trigger( 'squareMeter88', 2000 ); // 发布 88 平方米房子的价格
-salesOffices.trigger( 'squareMeter110', 3000 ); // 发布 110 平方米房子的价格
